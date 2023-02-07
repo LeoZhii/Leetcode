0c56ce9981eee2d6fbac015281ffe2870743396c
Leo Zhi <leozhi6869@gmail.com>
2023-02-06 19:03:12 -0800

    strtok is hurting my brain

--- CompetitiveProgramming/2023Spring/Contest5/A-ColdputerScience.c
+++ CompetitiveProgramming/2023Spring/Contest5/A-ColdputerScience.c
@@ -1,1 +1,26 @@
 #include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    // recieve inputs
+    int n;
+    char temperature[50];
+    scanf("%d", &n);
+    scanf("%s", temperature);
+
+    // get first token
+    char * token = strtok(temperature, " ");
+   
+    // iterate through remaining tokens
+    while( token != NULL ) {
+        printf( "%s\n", token );
+        
+        token = strtok(NULL, " ");
+    }
+
+    return 0;
+}
+
+
+// gcc -o A-ColdputerScience A-ColdputerScience.c
+// ./A-ColdputerScience
