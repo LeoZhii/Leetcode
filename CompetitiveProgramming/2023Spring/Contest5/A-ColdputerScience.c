a9999c206ae5f2b9e602192144bc962723338d49
Leo Zhi <leozhi6869@gmail.com>
2023-02-09 15:12:06 -0800

    Comp Prog Push

--- CompetitiveProgramming/2023Spring/Contest5/A-ColdputerScience.c
+++ CompetitiveProgramming/2023Spring/Contest5/A-ColdputerScience.c
@@ -3,21 +3,21 @@
 
 int main(void) {
     // recieve inputs
-    int n;
+    // int n;
+    // scanf("%d", &n);
     char temperature[50];
-    scanf("%d", &n);
     scanf("%s", temperature);
 
     // get first token
     char * token = strtok(temperature, " ");
-   
+    
+    int i = 0;
     // iterate through remaining tokens
-    while( token != NULL ) {
-        printf( "%s\n", token );
-        
-        token = strtok(NULL, " ");
+    while( token[i] != 0 ) {
+        printf( "%c\n", token[i] );
     }
 
+    
     return 0;
 }
 
