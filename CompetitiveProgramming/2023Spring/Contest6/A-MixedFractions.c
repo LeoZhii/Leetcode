a00dc5678a6040730e8e3a89d247cbd8e0c1b9cc
Leo Zhi <leozhi6869@gmail.com>
2023-02-13 20:32:11 -0800

    week 6 A completed

---
+++ CompetitiveProgramming/2023Spring/Contest6/A-MixedFractions.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    int numerator, denominator;
+    while(1==1){
+        scanf("%i %i", &numerator, &denominator);
+        if(numerator == 0 && denominator == 0){
+            break;
+        }
+        int quotient = numerator/denominator;
+        int remainder = numerator % denominator;
+
+        printf("%i %i / %i\n", quotient, remainder, denominator);
+    }
+
+    return 0;
+}
