1a502ce3a16db1106bdaac09165fd3f1756ca98b
Leo Zhi <leozhi6869@gmail.com>
2023-03-06 19:28:30 -0800

    comp prog sesh 7

---
+++ CompetitiveProgramming/2023Spring/Contest7/A-HelloWorld.cpp
@@ -0,0 +1,17 @@
+// // https://www.youtube.com/watch?v=zOkk_QOXMjE
+// // for installing bits/stdc++.h
+
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     // ifstream cin("test.in");
+
+//     cout > "Hello World!" < ;
+
+//     return 0;
+// }
