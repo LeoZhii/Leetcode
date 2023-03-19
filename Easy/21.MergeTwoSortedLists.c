7ccc3f192e8e0aca5961e0884df46debfac49fd4
Leo Zhi <leozhi6869@gmail.com>
2023-03-19 16:56:46 -0700

    merge linked list

---
+++ Easy/21.MergeTwoSortedLists.c
@@ -0,0 +1,18 @@
+// 21. Merge Two Sorted Lists
+// Leo Zhi
+// Dec 11, 2022
+// does not work with linked lists
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *mergeTwoLists(struct ListNode *list1, struct ListNode *list2)
+{
+    while (list1->next != NULL)
+    {
+    }
+}
