75f04f9e0831fc8cd451c8676278c1cf8be46edb
Leo Zhi <leozhi6869@gmail.com>
2023-04-12 14:02:48 -0700

    complete #21. Merge Two Sorted Linked Lists

--- Easy/21.MergeTwoSortedLists.c
+++ Easy/21.MergeTwoSortedLists.c
@@ -2,17 +2,77 @@
 // Leo Zhi
 // Dec 11, 2022
 // does not work with linked lists
+// April 12, 2023
+// works with both c and c++!!
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
+#include <stdio.h>
+
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    struct ListNode *next;
+};
+
 struct ListNode *mergeTwoLists(struct ListNode *list1, struct ListNode *list2)
 {
-    while (list1->next != NULL)
+    if (list1 == NULL)
     {
+        return list2;
     }
+    else if (list2 == NULL)
+    {
+        return list1;
+    }
+
+    // allocate memory to head pointer
+    struct ListNode *head = malloc(sizeof(struct ListNode));
+
+    // create copy of head pointer that will iterate through the lists
+    struct ListNode *temp = head;
+
+    // compare first elements for initializing head
+    if (list1->val <= list2->val)
+    { // if first value is less than second
+        temp->val = list1->val;
+        list1 = list1->next;
+    }
+    else
+    { // if second value is less than first
+        temp->val = list2->val;
+        list2 = list2->next;
+    }
+
+    // iterate through the rest of the list
+    while (list1 != NULL && list2 != NULL)
+    {
+        // allocate memory
+        temp->next = malloc(sizeof(struct ListNode));
+        // increment temp
+        temp = temp->next;
+
+        // compare two pointer values
+        if (list1->val <= list2->val)
+        { // if first value is less than second
+            temp->val = list1->val;
+            list1 = list1->next;
+        }
+        else
+        { // if second value is less than first
+            temp->val = list2->val;
+            list2 = list2->next;
+        }
+    }
+
+    // if one list ends before the other, append the rest of the other list
+    if (list1 == NULL)
+    {
+        temp->next = list2;
+    }
+    else
+    {
+        temp->next = list1;
+    }
+
+    return head;
 }
