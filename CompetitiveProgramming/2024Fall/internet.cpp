8d87933e8cef3ca9e526e1ef34292a89dc248727
Leo Zhi <leozhi6869@gmail.com>
2024-12-06 12:50:26 -0800

    a bunch of files

---
+++ CompetitiveProgramming/2024Fall/internet.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string input; // Declare a string variable to hold the input
+
+    cin >> input; // Read input from the user
+
+    return 0; // Return 0 to indicate successful execution
+}
